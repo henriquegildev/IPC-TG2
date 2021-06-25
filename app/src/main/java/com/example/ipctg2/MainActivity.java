1da84dfa03063c89e11a46f965a2a52a9100703f
GitGilDev <henriquegildev@gmail.com>
2021-06-26 00:51:30 +0100

    Added Menu Items, TextViews are now clickable.
    //TODO:
    //-Increase clickable area of Menu Text-buttons

--- app/src/main/java/com/example/ipctg2/MainActivity.java
+++ app/src/main/java/com/example/ipctg2/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.ipctg2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,19 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         menu_button = (ImageButton) findViewById(R.id.menu_button3);
-        menu_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMenu();
-            }
-        });
+        menu_button.setOnClickListener(v -> openMenu(v));
     }
 
-    public void openMenu(){
-        Intent intent = new Intent(this, Menu.class);
-        startActivity(intent);
+    public void openMenu(View view) {
+        startActivity(new Intent(this, Menu.class));
     }
 }
--- app/src/main/java/com/example/ipctg2/Menu.java
+++ app/src/main/java/com/example/ipctg2/Menu.java
@@ -2,7 +2,11 @@ package com.example.ipctg2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView medButton = (TextView)findViewById(R.id.medicamentosButton);
+        medButton.setOnClickListener(v -> openMeds(v));
+        TextView addMedsButton = (TextView)findViewById(R.id.addMedsButton);
+        addMedsButton.setOnClickListener(v -> openAddMeds(v));
+        TextView settingsButton = (TextView)findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(v -> openSettings(v));
     }
+
+    public void openMeds(View view){
+        startActivity(new Intent(this, Medicamentos.class));
+    }
+    public void openAddMeds(View view){
+        startActivity(new Intent(this, Adicionar_Medicamentos.class));
+    }
+    public void openSettings(View view){
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
 }
