1a0cc30d3b1c8ca82526f62d43154d361e2b3df4
19932-po2 <63853725+19932-po2@users.noreply.github.com>
2021-06-25 22:05:29 +0100

    some_things5

--- app/src/main/java/com/example/ipctg2/MainActivity.java
+++ app/src/main/java/com/example/ipctg2/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.ipctg2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private BackButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addFile();
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainPage();
+            }
+        });
     }
 
     //TODO: Updated files are done.
