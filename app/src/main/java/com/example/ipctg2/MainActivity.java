6dddb0ce60cb407400ebbaf4d497d7a6c1b59ede
GitGilDev <henriquegildev@gmail.com>
2021-06-25 23:26:26 +0100

    Fixed SDK Version incompatibility

--- app/src/main/java/com/example/ipctg2/MainActivity.java
+++ app/src/main/java/com/example/ipctg2/MainActivity.java
@@ -2,30 +2,30 @@ package com.example.ipctg2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private BackButton button;
+    private Button menu_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFile();
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        menu_button = (Button) findViewById(R.id.menu_button);
+        menu_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMainPage();
+                openMenu();
             }
         });
     }
 
-    //TODO: Updated files are done.
-    void addFile(){
-        //Empty
-
+    public void openMenu(){
+        Intent intent = new Intent(this, activity_menu);
     }
 }
---
+++ app/src/main/java/com/example/ipctg2/Menu.java
@@ -0,0 +1,14 @@
+package com.example.ipctg2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
