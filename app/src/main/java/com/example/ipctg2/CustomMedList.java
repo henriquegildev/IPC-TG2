0219361fb548e389793c0b821c93012b42f06db8
devgabrielgomes <63853725+19932-po2@users.noreply.github.com>
2021-06-26 16:27:45 +0100

    vomit100

---
+++ app/src/main/java/com/example/ipctg2/CustomMedList.java
@@ -0,0 +1,36 @@
+package com.example.ipctg2;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomMedList extends ArrayAdapter {
+    private String[] medNames;
+    private Integer[] imageid;
+    private Activity context;
+
+    public CustomMedList(Activity context, String[] medNames, Integer[] imageid) {
+        super(context, R.layout.row_icons_med_items, medNames);
+        this.context = context;
+        this.medNames = medNames;
+        this.imageid = imageid;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        LayoutInflater inflater = context.getLayoutInflater();
+        if(convertView==null)
+            row = inflater.inflate(R.layout.row_icons_med_items, null, true);
+        TextView textViewNameMed = (TextView) row.findViewById(R.id.textView_name_med);
+        ImageView medIcon = (ImageView) row.findViewById(R.id.imageViewFlag);
+
+        textViewNameMed.setText(medNames[position]);
+        medIcon.setImageResource(imageid[position]);
+        return  row;
+    }
+}
--- app/src/main/java/com/example/ipctg2/SelectMedIcon.java
+++ app/src/main/java/com/example/ipctg2/SelectMedIcon.java
@@ -2,11 +2,25 @@ package com.example.ipctg2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
-public class SelectMedIcon extends AppCompatActivity {
+public class SelectMedIcon extends ListActivity {
     private ImageButton backButton;
+    private ListView listView;
+    private String nameMed[] = {
+            "Kestine", "Paracetamol", "Ibuprofeno", "Voltaren", "Klacid"
+    }
+
+    private Integer imageid[] = {
+            R.drawable.med1,
+            R.drawable.med2,
+            R.drawable.med3,
+            R.drawable.med4,
+            R.drawable.med5,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
