af9a1a9a490162b7ea60300601ec22ecd0e69351
GitGilDev <henriquegildev@gmail.com>
2021-06-26 13:45:07 +0100

    Small tweak.
    Signed-off-by: GitGilDev <henriquegildev@gmail.com>

--- app/src/main/java/com/example/ipctg2/MainActivity.java
+++ app/src/main/java/com/example/ipctg2/MainActivity.java
@@ -19,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menu_button = (ImageButton) findViewById(R.id.menu_button3);
+        menu_button = findViewById(R.id.menu_button3);
         menu_button.setOnClickListener(v -> openMenu(v));
 
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listStrings);
+        adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listStrings);
     }
 
     //TODO:
