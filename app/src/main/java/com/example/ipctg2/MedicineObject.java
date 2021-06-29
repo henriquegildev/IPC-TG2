fe8780820ad04d5290ce37759e1d8ec9404bfc84
henriquegildev <74499230+henriquegildev@users.noreply.github.com>
2021-06-29 18:57:35 +0100

    Updated MedicineObject MainActivity & SelectMedicineIcon

--- app/src/main/java/com/example/ipctg2/MedicineObject.java
+++ app/src/main/java/com/example/ipctg2/MedicineObject.java
@@ -2,7 +2,6 @@ package com.example.ipctg2;
 
 import android.icu.text.DateFormat;
 
-import java.time.Instant;
 import java.util.Date;
 import java.util.ArrayList;
 
@@ -13,12 +12,19 @@ public class MedicineObject {
     private String beginDate;
     private String endDate;
     private int[] pillAmount;
+    private Integer[] imageid = {
+            R.mipmap.med1,
+            R.mipmap.med2,
+            R.mipmap.med3,
+            R.mipmap.med4,
+            R.mipmap.med5,
+    };
     public MedicineObject(String name, int id, boolean taken, String startDate, String finishDate) {
-        medicineName = name;
-        mAmount = id;
-        tookPill = taken;
-        beginDate = startDate;
-        endDate = finishDate;
+        this.medicineName = name;
+        this.mAmount = id;
+        this.tookPill = taken;
+        this.beginDate = startDate;
+        this.endDate = finishDate;
     }
 
     public String getName() {
@@ -34,7 +40,7 @@ public class MedicineObject {
     public static ArrayList<MedicineObject> createMedsList(int[] defaultPills, String[] name, boolean taken) {
         ArrayList<MedicineObject> medicine = new ArrayList<MedicineObject>();
         String currentTimeString = DateFormat.getTimeInstance(DateFormat.DAY_OF_WEEK_IN_MONTH_FIELD).format(new Date());
-        String nextTimeString = DateFormat.getDateTimeInstance().toString();
+        String nextTimeString = "30-07-2021";
         String startDate = currentTimeString;
         String finishDate = nextTimeString;
 
